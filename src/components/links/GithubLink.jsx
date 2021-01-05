0769d8662642723ba7384a7d964f375321840680
Mathias <mathias@mathiashsteffensen@protonmail.com>
2021-01-05 22:18:33 +0100

    quick fix

--- src/components/links/GithubLink.jsx
+++ src/components/links/GithubLink.jsx
@@ -4,5 +4,5 @@ import GithubLogo from '../../assets/github-logo.svg'
 
 export default function GithubLink() 
 {
-    return <a className="link-light" target="_blank" href="https://github.com/mathiashsteffensen">my GitHub <img style={{height: '20px', marginLeft: '0.2rem'}} src={GithubLogo} alt="github link"/></a>
+    return <a className="link-light" target="_blank" rel="noreferrer" href="https://github.com/mathiashsteffensen">my GitHub <img style={{height: '20px', marginLeft: '0.2rem'}} src={GithubLogo} alt="github link"/></a>
 }
